85e87a41689bd370c78559c6cdecf39d3203c878
michele <mbarrant@iu.edu>
2025-07-03 11:55:12 -0400

    init

---
+++ src/main/java/com/movieAI/moviematcher/MoviematcherApplication.java
@@ -0,0 +1,13 @@
+package com.movieAI.moviematcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviematcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviematcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movieAI/moviematcher/MoviematcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieAI.moviematcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviematcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
